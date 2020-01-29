3947749c2e0e11adb850d304956c5a61a7158c71
donnajharris <donnajharris@gmail.com>
2020-01-29 09:57:40 -0500

    The rest of the in class example

---
+++ Week3ClassDemo/Controllers/ArticleController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week3ClassDemo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Week3ClassDemo.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var articles = new[]
+            {
+                new Article {
+                    Author = "Donna",
+                    Title = "My Example Article Title",
+                    Body = "These are a lot of important words for an article.",
+                    DateCreated = DateTime.Now
+                },
+
+                new Article {
+                    Author = "Donna H",
+                    Title = "A Second Article",
+                    Body = "More words are great.",
+                    DateCreated = DateTime.Now
+                }
+
+            };
+
+
+            return View(articles);
+        }
+
+        public IActionResult Example()
+        {
+
+            Article article = new Article
+            {
+                Author = "Donna H",
+                Title = "My Single Example Article",
+                Body = "These are more important words for an article.",
+                DateCreated = DateTime.Now
+            };
+
+            return View(article);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Article article)
+        {
+            article.DateCreated = DateTime.Now;
+
+            return View(article);
+        }
+
+
+    }
+}
