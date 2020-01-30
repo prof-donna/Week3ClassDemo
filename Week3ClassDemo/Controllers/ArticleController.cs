d410dcf62d4d764d5ed967b2fa9a2cfb593bfbb7
donnajharris <donnajharris@gmail.com>
2020-01-30 16:49:31 -0500

    Added ArticleController and Index view

---
+++ Week3ClassDemo/Controllers/ArticleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week3ClassDemo.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Week3ClassDemo.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            Article article = new Article
+            {
+                Author = "Donna",
+                Title = "My very first article",
+                Body = "Here is the text of my very first article",
+                DateCreated = DateTime.Now
+
+            };
+
+            return View(article);
+        }
+    }
+}
